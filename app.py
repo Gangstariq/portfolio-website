3b9d60b0d7b470ba16f31b4d561c9aaf15d41efe
gangstariq <tariq@moollan.net>
2026-02-14 21:59:35 +1100

    Added navigation card links and some pages

--- app.py
+++ app.py
@@ -6,5 +6,29 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/anime')
+def anime():
+    return render_template('anime.html')
+
+@app.route('/languages')
+def languages():
+    return render_template('languages.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
